2717c79c575f5866378aa53ddd45b8b69aade6a3
SuperWeebTanks <trilleo12@live.dk>
2019-03-05 13:12:06 +0100

    Added Transponder class

---
+++ SWT25_Assignment2_AirTrafficMonitoring/TransponderReceiver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT25_Assignment2_AirTrafficMonitoring
+{
+    public class RawTransponderDataEventArgs : EventArgs
+    {
+        public RawTransponderDataEventArgs(List<string> transponderData)
+        {
+            TransponderData = transponderData;
+        }
+        public List<string> TransponderData { get; }
+    }
+    public interface ITransponderReceiver
+    {
+        event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+    }
+
+    /*
+    public class TransponderReceiverFactory
+    {
+        public static ITransponderReceiver CreateTransponderDataReceiver()
+    }
+    */
+
+}
