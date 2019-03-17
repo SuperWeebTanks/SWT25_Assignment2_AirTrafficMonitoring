6e1862fbcdbf189e16b0538d4bb2bae2d6555ef5
MH <mathhan01@gmail.com>
2019-03-17 13:52:06 +0100

    Added test case for occurence detector

--- Undervisningsmateriale/TransponderReceiverUser.Test.Unit/TestTransponderReceiverClient.cs
+++ Undervisningsmateriale/TransponderReceiverUser.Test.Unit/TestTransponderReceiverClient.cs
@@ -1,38 +1,38 @@
-﻿using System.Collections.Generic;
-using NSubstitute;
-using NUnit.Framework;
-using SWT25_Assignment2_AirTrafficMonitoring;
-using TransponderReceiver;
+﻿//using System.Collections.Generic;
+//using NSubstitute;
+//using NUnit.Framework;
+//using SWT25_Assignment2_AirTrafficMonitoring;
+//using TransponderReceiver;
 
-namespace TransponderReceiverUser.Test.Unit
-{
-    public class TestTransponderReceiverClient
-    {
-        private ITransponderReceiver _fakeTransponderReceiver;
-        private TransponderReceiverClient _uut;
-        [SetUp]
-        public void Setup()
-        {
-            // Make a fake Transponder Data Receiver
-            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
-            // Inject the fake TDR
-            _uut = new TransponderReceiverClient(_fakeTransponderReceiver);
-        }
+//namespace TransponderReceiverUser.Test.Unit
+//{
+//    public class TestTransponderReceiverClient
+//    {
+//        private ITransponderReceiver _fakeTransponderReceiver;
+//        private TransponderReceiverClient _uut;
+//        [SetUp]
+//        public void Setup()
+//        {
+//            // Make a fake Transponder Data Receiver
+//            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+//            // Inject the fake TDR
+//            _uut = new TransponderReceiverClient(_fakeTransponderReceiver);
+//        }
 
-        [Test]
-        public void TestReception()
-        {
-            // Setup test data
-            List<string> testData = new List<string>();
-            testData.Add("ATR423;39045;12932;14000;20151006213456789");
-            testData.Add("BCD123;10005;85890;12000;20151006213456789");
-            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+//        [Test]
+//        public void TestReception()
+//        {
+//            // Setup test data
+//            List<string> testData = new List<string>();
+//            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+//            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+//            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
 
-            // Act: Trigger the fake object to execute event invocation
-            _fakeTransponderReceiver.TransponderDataReady
-                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+//            // Act: Trigger the fake object to execute event invocation
+//            _fakeTransponderReceiver.TransponderDataReady
+//                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
 
-            // Assert something here or use an NSubstitute Received
-        }
-    }
-}
+//            // Assert something here or use an NSubstitute Received
+//        }
+//    }
+//}
