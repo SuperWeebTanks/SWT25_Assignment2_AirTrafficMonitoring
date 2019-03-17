9259c5c4a0bb4565250f48466b7a33449faddc27
Martin <martinx1995x@hotmail.com>
2019-03-17 12:47:26 +0100

    begyndt på airtraffic monitor

--- SWT25_Assignment2_AirTrafficMonitoring/AirTrafficMonitor/Air Traffic Monitor.cs
+++ SWT25_Assignment2_AirTrafficMonitoring/AirTrafficMonitor/Air Traffic Monitor.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SWT25_Assignment2_AirTrafficMonitoring.Airport;
+using SWT25_Assignment2_AirTrafficMonitoring.DecodeFactory;
+
 
 namespace SWT25_Assignment2_AirTrafficMonitoring.AirTrafficMonitor
 {
     public abstract class Air_Traffic_Monitor
     {
+        private List<Track> tracks;
+        private IOccurenceDetector occurenceDetecter;
+        private IDisplay display;
+        protected Air_Traffic_Monitor(Airport.Airport airport, IOccurenceDetector detector,IDisplay display)
+        {
+            tracks=new List<Track>();
+            this.display = display;
+            occurenceDetecter = detector;
+            airport.TrackDataEvent += Update;
+        }
+
+
+        public void Update(Object sender, TrackDataEventArgs e)
+        {
+            var list = e.TrackData;
+            foreach (var track in list)
+            {
+                var listOfTracksInOccurence=//kald til IOccurence funktion 
+
+
+            }
+        }
+
+        
+        
     }
 
     public class Commercial_ATM : Air_Traffic_Monitor
