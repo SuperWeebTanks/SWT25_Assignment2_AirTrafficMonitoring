5a7fbc69cc99545b4eeee44f17597d772199b1d8
MH <mathhan01@gmail.com>
2019-03-20 09:41:18 +0100

    Added test cases to occurence detector

---
+++ AirTrafficMonitor.Unit.Test/FakeTrack.cs
@@ -0,0 +1,12 @@
+﻿using SWT25_Assignment2_AirTrafficMonitoring.DecodeFactory;
+
+namespace OccurrenceDetector.Test.Unit
+{
+    public class FakeTrack : Track
+    {
+        public FakeTrack()
+        {
+            AircraftType = "AirCraftStub";
+        }
+    }
+}
--- AirTrafficMonitor.Unit.Test/OccurenceDetector.cs
+++ AirTrafficMonitor.Unit.Test/OccurenceDetector.cs
@@ -5,42 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
+using OccurrenceDetector.Test.Unit;
 using SWT25_Assignment2_AirTrafficMonitoring.AirTrafficMonitor;
 using SWT25_Assignment2_AirTrafficMonitoring.DecodeFactory;
 
-namespace AirTrafficMonitor.Unit.Test
+namespace OccurrenceDetector.Unit.Test
 {
     public class OccurenceDetector
     {
             private IOccurenceDetector _uut;
             private OccurrenceEventArgs _receivedOccurenceEventArgs;
             private Track _observedTrack;
-            private List<Track> _occurenceTracks;
             private Track _occurenceTrack;
-
+            private List<Track> _occurenceTracks;
+            
             [SetUp]
             public void SetUp()
             {
                 _receivedOccurenceEventArgs = null;
                 _uut = new Commercial_OD();
-                _observedTrack = new CommercialTrack();
-                _occurenceTrack = new CommercialTrack();
+                _observedTrack = new FakeTrack();
+                _occurenceTrack = new FakeTrack();
                 _occurenceTracks = new List<Track>();
 
-                _uut.OccurenceDetectedEvent +=
+                _observedTrack.Tag = "Track1";
+                _occurenceTrack.Tag = "Track2";
+
+                _observedTrack.CurrentAltitude = 1000;
+                _observedTrack.CurrentPositionX = 5000;
+                _observedTrack.CurrentPositionY = 5000;
+
+               _uut.OccurenceDetectedEvent +=
                     (o, args) => { _receivedOccurenceEventArgs = args; };
             }
 
             [Test]
-            public void OccurenceDetector_OccurenceTracks_EventFired()
+            public void OccurenceDetector_Occurence_EventFired()
             {
-                _observedTrack.Tag = "Test01";
-                _observedTrack.CurrentAltitude = 400;
-                _observedTrack.CurrentPositionX = 5000;
-                _observedTrack.CurrentPositionY = 5000;
-                
-                _occurenceTrack.Tag = "Test02";
-                _occurenceTrack.CurrentAltitude = 400;
+                _occurenceTrack.CurrentAltitude = 1200;
                 _occurenceTrack.CurrentPositionX = 5000;
                 _occurenceTrack.CurrentPositionY = 5000;
                 _occurenceTracks.Add(_occurenceTrack);
@@ -48,6 +50,47 @@ namespace AirTrafficMonitor.Unit.Test
                 _uut.CheckOccurrence(_observedTrack, _occurenceTracks);
 
                 Assert.That(_receivedOccurenceEventArgs, Is.Not.Null);
+            }
+
+            [Test]
+            public void OccurenceDetector_NoOccurence_EventNotFired()
+            {
+                _occurenceTrack.CurrentAltitude = 18000;
+                _occurenceTrack.CurrentPositionX = 32000;
+                _occurenceTrack.CurrentPositionY = 72000;
+                _occurenceTracks.Add(_occurenceTrack);
+
+                _uut.CheckOccurrence(_observedTrack, _occurenceTracks);
+
+                Assert.That(_receivedOccurenceEventArgs, Is.Null);
+            }
+
+            [Test]
+            public void OccurenceDetector_Occurence_CorrectObservedTrackReceived()
+            {
+                _occurenceTrack.CurrentAltitude = 1200;
+                _occurenceTrack.CurrentPositionX = 5000;
+                _occurenceTrack.CurrentPositionY = 5000;
+                _occurenceTracks.Add(_occurenceTrack);
+
+                _uut.CheckOccurrence(_observedTrack, _occurenceTracks);
+
+                Assert.That(_receivedOccurenceEventArgs.ObservedTrack.Tag,
+                    Is.EqualTo("Track1"));
+            }
+
+            [Test]
+            public void OccurrenceDetector_Occurence_CorrectOccurenceTrackReceived()
+            {
+                _occurenceTrack.CurrentAltitude = 1000;
+                _occurenceTrack.CurrentPositionX = 7000;
+                _occurenceTrack.CurrentPositionY = 9000;
+                _occurenceTracks.Add(_occurenceTrack);
+
+                _uut.CheckOccurrence(_observedTrack, _occurenceTracks);
+
+                Assert.That(_receivedOccurenceEventArgs.OccurenceTrack.Tag,
+                    Is.EqualTo("Track2"));
             }
     }
 }
