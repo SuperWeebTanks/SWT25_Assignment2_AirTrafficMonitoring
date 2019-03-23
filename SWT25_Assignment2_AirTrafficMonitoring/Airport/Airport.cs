49b6bb21c5c756307766dfdb66502fb30bfa4ac7
Martin <martinx1995x@hotmail.com>
2019-03-23 18:09:50 +0100

    airport færdig testet

--- SWT25_Assignment2_AirTrafficMonitoring/Airport/Airport.cs
+++ SWT25_Assignment2_AirTrafficMonitoring/Airport/Airport.cs
@@ -6,9 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using SWT25_Assignment2_AirTrafficMonitoring.DecodeFactory;
 
-namespace SWT25_Assignment2_AirTrafficMonitoring.Airport
+namespace SWT25_Assignment2_AirTrafficMonitoring
 {
-    public class Airport : ISignalForwarder
+    public struct AirSpace
+    {
+        public int Height_to { get; set; }
+        public int Height_from{ get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    public class Airport
     {
         /// <summary>
         /// Airport Constructor
@@ -21,20 +28,20 @@ namespace SWT25_Assignment2_AirTrafficMonitoring.Airport
         /// <param name="factory"></param>
         /// <param name="forwarder"></param>
         #region Constructors 
-        public Airport(ITransponderReceiver receiver, 
-            ISignalForwarder forwarder, CommercialTrackFactory decode)
+        public Airport(ITransponderReceiver receiver,DecodeFactory.DecodeFactory decode, AirSpace airspace)
         {
             Decode = decode;
             Receiver = receiver;
             Receiver.TransponderDataReady += AirportReceiverHandler;
-            Forwarder = forwarder; 
+            this.airspace = airspace;
+
         }
         #endregion
         #region Properties
 
-        public CommercialTrackFactory Decode { get; private set; }
+        public DecodeFactory.DecodeFactory Decode { get; private set; }
         public ITransponderReceiver Receiver { get; private set; }
-        public ISignalForwarder Forwarder { get; private set; }
+        public AirSpace airspace { get; private set; }
  
 
         #endregion
@@ -42,13 +49,34 @@ namespace SWT25_Assignment2_AirTrafficMonitoring.Airport
         public void AirportReceiverHandler(object sender, RawTransponderDataEventArgs e)
         {
             var list = Decode.CreateTracks(e.TransponderData);
-           
-            TrackDataEvent?.Invoke(this, new TrackDataEventArgs(list));
+            var filteredList = FilterTracks(list);
+            
+            TrackDataEvent?.Invoke(this, new TrackDataEventArgs(filteredList));
         }
 
        public event EventHandler<TrackDataEventArgs> TrackDataEvent;
 
-        }
+       public List<Track> FilterTracks(List<Track> tracks)
+       {
+           List<Track> sendTracks=new List<Track>();
+           foreach (var track in tracks)
+           {
+               if (airspace.Height_from <= track.CurrentAltitude && airspace.Height_to >= track.CurrentAltitude
+                                                                 && airspace.X >= track.CurrentPositionX 
+                                                                 && airspace.Y >= track.CurrentPositionY
+                                                                 && track.CurrentPositionX>=0
+                                                                 && track.CurrentPositionY>=0)
+               {
+                    sendTracks.Add(track);
+               }
+           }
+
+           return sendTracks;
+       }
+    }
+
+
+
     public class TrackDataEventArgs : EventArgs
     {
         public TrackDataEventArgs(List<Track> trackData)
@@ -56,5 +84,9 @@ namespace SWT25_Assignment2_AirTrafficMonitoring.Airport
             TrackData = trackData;
         }
         public List<Track> TrackData { get; set; }
+
+
     }
+
+    
 }
--- UnitTests/AirportUnitTests.cs
+++ UnitTests/AirportUnitTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -20,8 +21,10 @@ namespace Airport.Unit.Tests
         private DecodeFactory _decoderMock;
         private ITransponderReceiver _transponderReceiverMock;
         private AirSpace _airspace;
+        private List<Track> _tracks;
+        private Track _track;
+        private List<Track> _trackEventArgs;
 
-        
         [SetUp]
         public void Setup()
         {
@@ -29,13 +32,144 @@ namespace Airport.Unit.Tests
             _decoderMock = Substitute.For<DecodeFactory>();
             _transponderReceiverMock = Substitute.For<ITransponderReceiver>();
             _airport=new SWT25_Assignment2_AirTrafficMonitoring.Airport(_transponderReceiverMock,_decoderMock,_airspace);
+            _track = new CommercialTrack
+            {
+                CurrentAltitude = 600,
+                CurrentCompassCourse = 200,
+                CurrentHorizontalVelocity = 300,
+                CurrentPositionX = 10000,
+                CurrentPositionY = 10000,
+                Tag = "aaaaaa",
+                TimeStamp = new DateTime(2020, 10, 10),
+            };
+            _trackEventArgs = null;
+            _tracks = new List<Track> { _track };
+
+            _airport.TrackDataEvent += (o, args) => { _trackEventArgs = args.TrackData; };
+
+        }
+
+        [Test]
+        public void TrackDataEvent_DoesFire()
+        {
+            
+            _decoderMock.CreateTracks(Arg.Any<List<string>>()).Returns(_tracks);
+        
+            _airport.AirportReceiverHandler(null, new RawTransponderDataEventArgs(new List<string> { "null" }));
+            Assert.That(_trackEventArgs,Is.EquivalentTo(_tracks));
         }
 
         [Test]
         public void FilterTracks_EmptyList()
         {
+            var tracks = new List<Track>();
+            var list=_airport.FilterTracks(tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void Filtertracks_HeightOverHighestLimit_NotAllowed()
+        {
+            _track.CurrentAltitude = 20001;
+          
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
 
         }
+
+        [Test]
+        public void FilterTracks_HeigtUnderHighestLimit_Allowed()
+        {
+            _track.CurrentAltitude = 20000;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void Filtertracks_HeightUnderLowestLimit_NotAllowed()
+        {
+            _track.CurrentAltitude = 499;
+
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+
+        }
+
+        [Test]
+        public void Filtertracks_HeightOverAndEqualToLowestLimit_Allowed()
+        {
+            _track.CurrentAltitude = 500;
+
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+
+        }
+
+        [Test]
+        public void FilterTracks_XCorOver80000_NotAllowed()
+        {
+            _track.CurrentPositionX = 80001;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_XCorUnderAndEqualTo8000_Allowed()
+        {
+            _track.CurrentPositionX = 80000;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_XCorUnder0_NotAllowed()
+        {
+            _track.CurrentPositionX = -1;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_XCorOverAndEqualTo0_Allowed()
+        {
+            _track.CurrentPositionX = 0;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_YCorOver80000_NotAllowed()
+        {
+            _track.CurrentPositionY = 80001;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_YCorUnderAndEqualTo80000_Allowed()
+        {
+            _track.CurrentPositionY = 80000;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_YCorUnder0_NotAllowed()
+        {
+            _track.CurrentPositionY =-1;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsTrue(list.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void FilterTracks_YCorOverAndEqualTo0_Allowed()
+        {
+            _track.CurrentPositionY = 0;
+            var list = _airport.FilterTracks(_tracks);
+            Assert.IsFalse(list.IsNullOrEmpty());
+        }
+
+
     }
 
     [TestFixture]
